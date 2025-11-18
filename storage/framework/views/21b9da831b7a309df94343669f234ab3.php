c1def3ab11ad2aa998aa9aebc460b9878ba65229
JoseAlexander1997 <josehernandezdelr1997@gmail.com>
2025-11-18 09:57:38 -0600

    correcciones generales por error

---
+++ storage/framework/views/21b9da831b7a309df94343669f234ab3.php
@@ -0,0 +1,3 @@
+<svg class="w-3 h-3 absolute opacity-0 group-hover:opacity-100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"/>
+</svg><?php /**PATH C:\xampp\htdocs\API_tipocambio_BANGUAT\storage\framework\views/f699071e3525642e63fdd115c7ade21d.blade.php ENDPATH**/ ?>
